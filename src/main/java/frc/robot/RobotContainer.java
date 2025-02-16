79aee575b16c2af6804e3efe9515e460faff2eb6
Rona Sando <ard@MacBook-Air-2.local>
2025-02-16 17:09:03 +0900

    ready to test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -50,7 +50,7 @@ public class RobotContainer
   public void configureBindings(){
     
     Command swerve_Command = drivebase.driveCommand(() -> driverXbox.getRawAxis(0),() -> driverXbox.getRawAxis(1),() -> driverXbox.getRawAxis(2),() -> driverXbox.getRawAxis(3));
-    //drivebase.setDefaultCommand(swerve_Command);
+    drivebase.setDefaultCommand(swerve_Command);
   }
 
 
