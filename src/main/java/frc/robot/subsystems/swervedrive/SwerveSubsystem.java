c2d739696027ad6163d41fa1512b4ac5e72685d9
Rona Sando <ard@MacBook-Air-2.local>
2025-03-09 19:18:48 +0900

    hello world again

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -100,44 +100,62 @@ public class SwerveSubsystem extends SubsystemBase
       return run(() -> swervedrive.lockPose());
     }
 
-    public Command drivetotarget (){
+    public Command drivetotarget() {
       return run(() -> {
-        RawDetection[] results = LimelightHelpers.getRawDetections("");
-          if(results.length > 0){
-            RawDetection tag = results[0];
-            double tx = tag.txnc;
-            double ty = tag.tync;
-
-            double area = tag.ta;
-
-            double top_left_x = tag.corner0_X;
-            double top_left_y = tag.corner0_Y;
-
-            double top_right_x = tag.corner1_X;
-            double top_right_y = tag.corner1_Y;
-
-            double bottom_left_x = tag.corner3_X;
-            double bottom_left_y = tag.corner3_Y;
-
-            double bottom_right_x = tag.corner2_X;
-            double bottom_right_y = tag.corner2_Y;
-
-            double dist_right = top_right_y - bottom_right_y;
-            double dist_left = top_left_y - bottom_left_y;
-
-            double rotation =  (dist_left - dist_right)/480*Math.PI/2;
-            double speed = 0.1 - area;
-            double angle = (tx - 320)/320*Math.PI/4;
-
-            Translation2d translation = new Translation2d(speed,new Rotation2d(angle));
-            ChassisSpeeds movement = swervedrive.swerveController.getTargetSpeeds(translation.getX(), translation.getY(),rotation,swervedrive.getOdometryHeading().getRadians(),swervedrive.getMaximumChassisVelocity());
-            swervedrive.drive(movement,new Translation2d(0, 0 ));
-
-
+          RawDetection[] results = LimelightHelpers.getRawDetections("");
+          if (results.length > 0) {
+              RawDetection tag = results[0];
+              double tx = tag.txnc;
+              double ty = tag.tync;
+              double area = tag.ta;
+  
+              double top_left_x = tag.corner0_X;
+              double top_left_y = tag.corner0_Y;
+              double top_right_x = tag.corner1_X;
+              double top_right_y = tag.corner1_Y;
+              double bottom_left_x = tag.corner3_X;
+              double bottom_left_y = tag.corner3_Y;
+              double bottom_right_x = tag.corner2_X;
+              double bottom_right_y = tag.corner2_Y;
+  
+              // Rotation calculation (still may need fine-tuning)
+              double dist_right = top_right_y - bottom_right_y;
+              double dist_left = top_left_y - bottom_left_y;
+              double rotation = (dist_left - dist_right) / 480 * Math.PI / 2; // adjust scaling factor as needed
+  
+              // Speed adjustment (consider refining this)
+              double speed = Math.max(0.00, 0.1 - area); // Avoiding negative speeds
+  
+              // Angle calculation (you might want to adjust the scaling factor here)
+              double angle = (tx - 320) / 320 * Math.PI / 4;
+  
+              // Translation2d used to control forward movement and rotation
+              Translation2d translation = new Translation2d(speed, new Rotation2d(angle));
+  
+              // Convert translation to chassis speeds
+              ChassisSpeeds movement = swervedrive.swerveController.getTargetSpeeds(
+                  translation.getX(),
+                  translation.getY(),
+                  rotation,
+                  swervedrive.getOdometryHeading().getRadians(),
+                  swervedrive.getMaximumChassisVelocity()
+              );
+  
+              // Drive the robot based on calculated speeds
+              swervedrive.drive(movement, new Translation2d(0, 0));
+  
+              // Optionally log data for debugging
+              SmartDashboard.putNumber("tx", tx);
+              SmartDashboard.putNumber("ty", ty);
+              SmartDashboard.putNumber("area", area);
+              SmartDashboard.putNumber("rotation", rotation);
+              SmartDashboard.putNumber("speed", speed);
+              SmartDashboard.putNumber("angle", angle);
           }
       });
-
-    }
+  }
+  
+  
 
 
 
